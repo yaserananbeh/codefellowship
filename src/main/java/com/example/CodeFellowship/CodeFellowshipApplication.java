12936e09eebef36e3d353bc62df96660ccad4bee
Yaser Al-Ananbeh <yaser.ananbeh74@gmail.com>
2021-08-16 15:46:31 +0300

    initalize the app using spring initializer and and the proper dependencies

---
+++ src/main/java/com/example/CodeFellowship/CodeFellowshipApplication.java
@@ -0,0 +1,13 @@
+package com.example.CodeFellowship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeFellowshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeFellowshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CodeFellowship/CodeFellowshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CodeFellowship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeFellowshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
